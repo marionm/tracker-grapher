071036bc8301b3d0dc5edf468ced149ec46e3bc6
Mike Marion <mike.marion@gmail.com>
2016-02-25 23:57:50 -0600

    Add React and Browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,37 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    paths: {
+      dist: 'app/dist',
+      scripts: 'app/scripts'
+    },
+
+    browserify: {
+      dist: {
+        files: {
+          '<%= paths.dist %>/tracker-grapher.js': '<%= paths.scripts %>/**/*.js'
+        },
+
+        options: {
+          transform: ['reactify']
+        }
+      }
+    },
+
+    clean: {
+      dist: ['<%= paths.dist %>']
+    },
+
+    watch: {
+      scripts: {
+        files: '<%= paths.scripts %>/**/*.js',
+        tasks: 'browserify'
+      }
+    }
   });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['browserify']);
 };
