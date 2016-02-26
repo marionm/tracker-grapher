a8a9b8e5cbcd49140e3a98f2b35a44504dd1ebbd
Mike Marion <mike.marion@gmail.com>
2016-02-25 23:55:02 -0600

    Add project model

---
+++ app/scripts/models/project.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Pivotal = require('./pivotal');
+const Story = require('./story');
+
+const Project = function(options) {
+  options = options || {};
+  this.id = options.id;
+  this.name = options.name;
+};
+
+Project.loadAll = function() {
+  if (Project.all) {
+    return Promise.resolve(Project.all);
+  }
+
+  return Pivotal.get('projects').then((response) => {
+    Project.all = response.data.map((data) => new Project(data));
+    return Project.all;
+  });
+};
+
+Project.prototype.search = function(query) {
+  return Pivotal.get(`projects/${this.id}/search`, {
+    params: { query }
+  }).then((response) => {
+    return response.data.stories.stories.map((storyData) => {
+      return new Story(storyData);
+    });
+  });
+};
+
+module.exports = Project;
