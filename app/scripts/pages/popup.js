a378c0a485fe353227827f6ad3c4dfce63e40bb9
Mike Marion <mike.marion@gmail.com>
2016-02-26 00:04:27 -0600

    Add simple popup

---
+++ app/scripts/pages/popup.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const ApiKeyInput = require('../components/api-key-input');
+const Pivotal = require('../models/pivotal');
+const React = require('react');
+const SearchInput = require('../components/search-input');
+
+const Popup = React.createClass({
+  getInitialState() {
+    return {
+      apiKey: null,
+      loading: true
+    }
+  },
+
+  componentDidMount() {
+    // FIXME: Ugh - why doesn't get().then(validate).then(ok).catch(fail) work?
+    Pivotal.getApiKey().then((apiKey) => {
+      if (apiKey) {
+        Pivotal.validateApiKey(apiKey).then(() => {
+          this.setState({ apiKey, loading: false });
+        }).catch(() => {
+          this.setState({ loading: false });
+        });
+      } else {
+        this.setState({ loading: false });
+      }
+    }).catch(() => {
+      this.setState({ loading: false });
+    });
+  },
+
+  onApiKeySet(apiKey) {
+    this.setState({ apiKey });
+  },
+
+  render() {
+    if (this.state.loading) {
+      return (<div>Loading...</div>);
+    } else if (this.state.apiKey) {
+      return (<SearchInput/>);
+    } else {
+      return (<ApiKeyInput onChange={this.onApiKeySet}/>);
+    }
+  }
+});
+
+module.exports = Popup;
--- app/scripts/tracker-grapher.js
+++ app/scripts/tracker-grapher.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const Popup = require('./pages/popup');
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const target = document.body.children[0];
+ReactDOM.render(<Popup/>, target);
